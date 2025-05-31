b7d0f0dd500228e61083c6aaf504f04dee6f2b28
rapo5654 <razanava.palina@gmail.com>
2025-05-31 16:57:29 +0300

    AppDatabase.java

--- AppDatabase.java
+++ AppDatabase.java
@@ -1,31 +1,33 @@
 package com.rapo.goalgetter.data.database;
 
 import android.content.Context;
+
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+
 import com.rapo.goalgetter.data.dao.TaskDao;
 import com.rapo.goalgetter.data.dao.UserDao;
 import com.rapo.goalgetter.data.model.Task;
 import com.rapo.goalgetter.data.model.User;
 
-@Database(entities = {User.class, Task.class}, version = 1, exportSchema = false)
+@Database(entities = {User.class, Task.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
-
     private static volatile AppDatabase INSTANCE;
-    private static final String DB_NAME = "goalgetter.db";
 
     public abstract UserDao userDao();
     public abstract TaskDao taskDao();
 
-    public static synchronized AppDatabase getInstance(Context context) {
+    public static AppDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
-            INSTANCE = Room.databaseBuilder(
-                            context.getApplicationContext(),
-                            AppDatabase.class,
-                            DB_NAME
-                    ).fallbackToDestructiveMigration()
-                    .build();
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "goalgetter_db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
         }
         return INSTANCE;
     }
